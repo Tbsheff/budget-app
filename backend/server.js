fc63a8dc983043724c80291a60f881675152ecce
bbundy12 <brendanbundy1@gmail.com>
2024-12-31 16:07:01 -0700

    statically running frontend with the backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const path = require('path'); // Import path module
 require('dotenv').config();
 
 const app = express();
@@ -20,6 +21,16 @@ app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/incomes', require('./routes/incomeRoutes'));
 app.use('/api/expenses', require('./routes/expenseRoutes'));
 app.use('/api/goals', require('./routes/goalRoutes'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    // Handle React routing, return all requests to React app
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
